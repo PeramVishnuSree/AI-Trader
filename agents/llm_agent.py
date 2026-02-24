53d8649207c951cd24503ac6312047bcec3b01e5
Vishnu Peram <vjp5266@psu.edu>
2026-02-23 17:12:24 -0800

    datatype errors debugged

--- agents/llm_agent.py
+++ agents/llm_agent.py
@@ -1,19 +1,20 @@
 import os
 from openai import OpenAI
 from dotenv import load_dotenv
+import random
 
 load_dotenv()
 
 client = OpenAI(api_key = os.getenv("OPENAI_API_KEY"))
 
 def summarize_market(data):
-    last_price = data.iloc[-1]["Close"]
-    avg_price = data["Close"].mean()
+    last_price = float(data.iloc[-1]["Close"])
+    avg_price = float(data["Close"].mean())
     change = (last_price - avg_price) / avg_price *100
 
-    print(type(data))
-    print(data.columns)
-    print(data.head())
+    # print(type(data))
+    # print(data.columns)
+    # print(data.head())
 
     return f""" 
             Last Price: {last_price:.2f}
@@ -22,25 +23,26 @@ def summarize_market(data):
             """ 
 
 def get_decision(summary: str):
-    prompt = f"""
-    You are a trading agent.
-    Based on this market summary:
+    # prompt = f"""
+    # You are a trading agent.
+    # Based on this market summary:
 
-    {summary}
+    # {summary}
 
-    Respond ONLY with one word:
-    BUY, SELL, or HOLD.
-    """
+    # Respond ONLY with one word:
+    # BUY, SELL, or HOLD.
+    # """
 
-    response = client.chat.completions.create(
-        model = "gpt-4.1-mini",
-        messages=[{"role": "user", "content": prompt}],
-        temperature=0
-    )
+    # response = client.chat.completions.create(
+    #     model = "gpt-4.1-mini",
+    #     messages=[{"role": "user", "content": prompt}],
+    #     temperature=0
+    # )
 
-    decision = response.choices[0].message.content.strip().upper()
+    # decision = response.choices[0].message.content.strip().upper()
 
-    if decision not in ["BUY", "SELL", "HOLD"]:
-        return "HOLD"
+    # if decision not in ["BUY", "SELL", "HOLD"]:
+    #     return "HOLD"
     
-    return decision
+    
+    return random.choice(["BUY", "SELL", "HOLD"])
--- data/data_loader.py
+++ data/data_loader.py
@@ -3,6 +3,12 @@ import pandas as pd
 
 def get_price_history(symbol: str, start: str, end: str) -> pd.DataFrame:
     df = yf.download(symbol, start=start, end=end)
-    df = df[['Close']]
+
+    #Handle multi index columns if present
+    if isinstance(df.columns, pd.MultiIndex):
+        df.columns = df.columns.get_level_values(0)
+
+    df = df[["Close"]]
     df.dropna(inplace=True)
+    
     return df
